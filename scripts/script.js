21f0f437a5756149a12ea6820879ebeca5309f79
ericlnewman <eric.leon.newman@gmail.com>
2024-01-17 21:05:38 -0500

    Updated javascript to make 16 sqaures within the div. Add some css styling within the JS.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
 /*
     JS for etch-a-sketch
 */
+const container = document.querySelector("#container");
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+function createDiv(){
+    for(let i = 1; i <= 16; i++){
+        const divVar = document.createElement("div");
+        divVar.style.width = "200px";
+        divVar.style.height = "200px";
+        divVar.style.backgroundColor = "aliceblue";
+        divVar.style.margin = "5px";
+        divVar.id = i;
+        container.appendChild(divVar);
+    }
+}
+window.onload = createDiv;
