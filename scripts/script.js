5824085ccc8f524ca475cbc1d72be5217965d26d
ericlnewman <eric.leon.newman@gmail.com>
2024-01-20 17:33:02 -0500

    updated flexbox styling

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,24 @@
     JS for etch-a-sketch
 */
 const container = document.querySelector("#container");
+const flexBox = document.createElement("div");
 
+const refreshBtn = document.createElement("button");
+
+
+
+refreshBtn.id = "btn";
+flexBox.id = "flex";
+refreshBtn.textContent = "Refresh Button";
+container.appendChild(refreshBtn);
+container.appendChild(flexBox);
 function createDiv(){
     const number = 16*16;
     for(let i = 1; i <= number; i++){
         const divVar = document.createElement("div");
         divVar.className = "gridBox";
         divVar.id = i;
-        container.appendChild(divVar);
+        flexBox.appendChild(divVar);
     }
 }
 window.onload = createDiv;
