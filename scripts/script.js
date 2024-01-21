d9901f06613a596ac04fbf8d6b59a578ee57f31f
ericlnewman <eric.leon.newman@gmail.com>
2024-01-21 17:09:40 -0500

    updated sizing of div's for grid to be perfectly fitting squares that can be dynamically changed

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,26 @@ flexBox.id = "flex";
 refreshBtn.textContent = "Refresh Button";
 container.appendChild(refreshBtn);
 container.appendChild(flexBox);
+let userInput;
 function createDiv(){
-    const number = 16*16;
-    for(let i = 1; i <= number; i++){
+    let number;
+    if(userInput !== undefined){
+        number = userInput;
+    } else {
+        number = 16;
+    }
+    const doubleNumber = number * number;
+    for(let i = 1; i <= doubleNumber; i++){
         const divVar = document.createElement("div");
         divVar.className = "gridBox";
-        divVar.id = i;
+        divVar.style.width = `${960/number}px`;
+        divVar.style.height = `${960/number}px`;
         flexBox.appendChild(divVar);
     }
-}
+}/*
+const divToChange = document.querySelector(".changeColor");
+divToChange.addEventListener("mouseover", ()=>{
+    divToChange.document.style.backgroundColor = "gray";
+});
+*/
 window.onload = createDiv;
