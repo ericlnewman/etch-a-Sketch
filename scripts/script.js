43b922bd94b0873798c37c639f43bd2f5a1d915a
ericlnewman <eric.leon.newman@gmail.com>
2024-01-23 16:13:51 -0500

    updated createDiv function to be within the button event, reason: not able to change div using window.onload. Added a means of changing the color for divs after being entered in by the user upon pressing the button, which is the create an array of the divs and then adding an event listener to each div.

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,26 @@
 */
 const container = document.querySelector("#container");
 const flexBox = document.createElement("div");
+const btn = document.createElement("button");
 
-const refreshBtn = document.createElement("button");
-
-
-
-refreshBtn.id = "btn";
+btn.id = "btn";
 flexBox.id = "flex";
-refreshBtn.textContent = "Refresh Button";
-container.appendChild(refreshBtn);
+btn.textContent = "Etch-a-Sketch Sqaures to add";
+container.appendChild(btn);
 container.appendChild(flexBox);
-let userInput;
-function createDiv(){
-    let number;
-    if(userInput !== undefined){
-        number = userInput;
-    } else {
-        number = 16;
+btn.addEventListener("click", ()=>{
+    console.log("the button was pressed")
+    const userInput = parseInt(prompt("How many squares would you like put in the Etch-a-Sketch?"));
+    if(userInput > 100 || isNaN(userInput) || userInput == undefined){
+        userInput = 16;
     }
+    console.log("and now it should execute the div function to refresh with the given number if the input is undefined");
+    console.log("the userInput is " + userInput);
+    let number = userInput;
+    console.log("number is " + number + ` and thus dividing 960 by it is ${960/number}`);
     const doubleNumber = number * number;
+    // clear the flexbox of previous elements:
+    flexBox.innerHTML = "";
     for(let i = 1; i <= doubleNumber; i++){
         const divVar = document.createElement("div");
         divVar.className = "gridBox";
@@ -29,10 +30,11 @@ function createDiv(){
         divVar.style.height = `${960/number}px`;
         flexBox.appendChild(divVar);
     }
-}/*
-const divToChange = document.querySelector(".changeColor");
-divToChange.addEventListener("mouseover", ()=>{
-    divToChange.document.style.backgroundColor = "gray";
+// How to change color of the divs:
+    // get the div elements created
+    let divs = document.getElementsByClassName("gridBox");
+    // form an array of each of these, and then add an event listener for the mouseover
+    Array.from(divs).forEach((div)=>{div.addEventListener("mouseover", function(){
+         this.style.backgroundColor = "aliceblue";
+    })});
 });
-*/
-window.onload = createDiv;
